0f207bb6067fa268d5f7415b0aa344f3aad373ce
niclas@unibe <scheuing@students.unibe.ch>
2015-01-13 18:44:48 +0100

    Working on config file

--- dronarch/helpers/debug.py
+++ dronarch/helpers/debug.py
@@ -8,7 +8,7 @@ __author__ = 'niclas'
 
 #logging levels
 levels = {0:'Debug', 1: 'Warning', 2: 'Error'}
-logfile = '../../config/dronarch.log'
+logfiles = ['../../roaming/dronarch.log','../../config/dronarch.log']
 def debug(level,*message):
     """
     All Debug and Error messages should be sent to this function.
@@ -21,17 +21,19 @@ def debug(level,*message):
     if level in levels.keys():
         output = levels[level],': ', msg
         print(levels[level],': ', msg)
-        write_to_file(logfile=logfile, message=' '.join(output))
+        write_to_file(logfiles=logfiles, message=' '.join(output))
     else:
         print('Invalid debug level {} for message "{}". Has to be one of {}'.format(level,msg,levels))
 
-def write_to_file(logfile, message):
-    with open(logfile, 'a+') as file:
-        timestamp = datetime.fromtimestamp(time()).strftime('%d.%m.%Y %H:%M:%S')
-        file.write(timestamp+': '+message+'\n')
+def write_to_file(logfiles, message):
+    for logfile in logfiles:
+        with open(logfile, 'a+') as file:
+            timestamp = datetime.fromtimestamp(time()).strftime('%d.%m.%Y %H:%M:%S')
+            file.write(timestamp+': '+message+'\n')
 
 def clear_log_file():
-    remove(logfile)
+    for logfile in logfiles:
+        remove(logfile)
 
 if __name__=='__main__':
     debug(1, 'some ', ' more ', 23)
